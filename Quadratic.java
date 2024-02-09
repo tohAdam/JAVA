d8ade4177c67391b90f12e7113175b8c3af1816b
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-09 10:43:48 +0800

    chapter 9

---
+++ Quadratic.java
@@ -0,0 +1,10 @@
+public class Quadratic
+{
+   public static void main( String[] args )
+   {
+      float x = 4;
+      float result = 3 * (x * x) - (8 * x) + 4;
+      System.out.println("At x = 4.0 the value is " + result);
+      
+   }
+}
