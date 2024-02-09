aff701ff5585568987ab67df8b8072c8bda7a0b7
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-09 14:54:40 +0800

    chapter 11

---
+++ StringDemo3.java
@@ -0,0 +1,13 @@
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String original = new String("    In a Hole    in the ground there lived a Hobbit.   ");
+        String trimmed;
+
+        // Call trim() method to create a new String with leading and trailing spaces trimmed
+        trimmed = original.trim();
+
+        // Print both original and trimmed Strings
+        System.out.println("Original string: \"" + original + "\"");
+        System.out.println("Trimmed string:  \"" + trimmed + "\"");
+    }
+}
