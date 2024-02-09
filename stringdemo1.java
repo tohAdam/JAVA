dc414f6130508191dd8b4a4174c6f041b51ac2fb
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-09 14:51:00 +0800

    chapter 11

---
+++ stringdemo1.java
@@ -0,0 +1,12 @@
+public class stringdemo1 {
+    public static void main(String[] args) {
+        // Create a String object containing "Hello World"
+        String str = "Hello World";
+
+        // Print the contents of the object referred to by str
+        System.out.println(str);
+
+        // Add a second reference variable without creating an object for it
+        String secondVariable;
+    }
+}
