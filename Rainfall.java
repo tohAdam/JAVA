ac9d35ae7babced552d388f9305ab2afc1886a41
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-09 10:45:04 +0800

    chapter 10

---
+++ Rainfall.java
@@ -0,0 +1,16 @@
+public class Rainfall
+{
+   public static void main(String[] args)
+   {
+      float April, May, June, average;
+      April = 12;
+      May = 14;
+      June = 8;
+      average = ( April + May + June ) / 3;
+      
+      System.out.println("Rainfall for April: "+ April);
+      System.out.println("Rainfall for May: "+ May);
+      System.out.println("Rainfall for June: "+ June);
+      System.out.println("Average Rainfall: "+ average);
+   }
+}
