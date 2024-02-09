d189b7db9537ad4cd24c39063fda2ab5e7dac3ab
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-09 10:45:53 +0800

    chapter 10

---
+++ Radiants.java
@@ -0,0 +1,10 @@
+public class Radiants
+{
+   public static void main( String[] args )
+   {
+      double rad;
+      int degrees = 50; //value
+      rad = degrees * Math.PI/180;
+      System.out.println("The radiant is: "+ rad);
+   }
+}
