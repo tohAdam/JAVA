11602131b995703b110e7e473d254e3ea6883de8
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-09 10:44:19 +0800

    chapter 9

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables
+{
+   public static void main( String[] args )
+   {
+      double x, result;
+      x = 0.0;
+      result = 3 * (x * x) - (8 * x) + 4;
+      System.out.println("At x = 0.0 the value is " + result);
+      
+      x = 2.0;
+      result = 3 * (x * x) - (8 * x) + 4;
+      System.out.println("At x = 0.0 the value is " + result);
+      
+      x = 4.0;
+      result = 3 * (x * x) - (8 * x) + 4;
+      System.out.println("At x = 0.0 the value is " + result);
+      
+   }
+}
