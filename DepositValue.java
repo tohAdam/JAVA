4eea3eeefa18db627e82c18a7a26c7c2bb307a08
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-21 22:43:29 +0800

    chapter 13

---
+++ DepositValue.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DepositValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+   System.out.println("Initial deposit:");
+        double P = scanner.nextDouble();
+
+   System.out.println("Interest rate:");
+        double r = scanner.nextDouble();
+
+   System.out.println("Times per year:");
+        double n = scanner.nextDouble();
+
+        System.out.println("Number of years:");
+        double t = scanner.nextDouble();
+        double V = P * Math.pow(1 + r / n, n * t);
+  System.out.printf("Value: $%.2f%n", V);
+
+        scanner.close();
+    }
+}
