cc1a103387ed7b08499bc972749c9f592f4ef026
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-21 22:35:44 +0800

    chapter 13

---
+++ ApplianceCost.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ApplianceCost {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter cost per kilowatt-hour in cents:");
+        double costPerKWh = scanner.nextDouble();
+
+        System.out.println("Enter kilowatt-hours used per year:");
+        double kWhPerYear = scanner.nextDouble();
+        double annualCost = costPerKWh * kWhPerYear / 100; 
+        System.out.println("Annual cost: " + annualCost);
+
+    }
+}
