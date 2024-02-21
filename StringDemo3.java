d95c7d49c7e18a775bc698c34e21960195f184f6
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-21 22:31:15 +0800

    chapter 11

--- StringDemo3.java
+++ StringDemo3.java
@@ -6,3 +6,63 @@ public class StringDemo3 {
         System.out.println(sub);
     }
 }
+//q1
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str = new String("Golf is a good walk spoiled."); 
+        String sub = str.substring(8, 8); 
+
+        System.out.println(sub);
+    }
+}
+//q2
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str = new String("Golf is a good walk spoiled."); 
+        String sub = str.substring(0, str.length()); 
+
+        System.out.println(sub);
+    }
+}
+//q3
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str = new String("Golf is a good walk spoiled.");
+        String sub = str.substring(0, str.length()); 
+
+        System.out.println("Substring: \"" + sub + "\"");
+        System.out.println("Length of substring: " + sub.length());
+    }
+}
+//q4
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str = new String("Golf is a good walk spoiled.");
+        String sub = str.substring(0, 29); 
+
+        System.out.println("Substring: \"" + sub + "\"");
+        System.out.println("Length of substring: " + sub.length());
+    }
+}
+//q5
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str = new String("Golf is a good walk spoiled.");
+        String sub = str.substring(-1, 29); 
+
+        System.out.println("Substring: \"" + sub + "\"");
+        System.out.println("Length of substring: " + sub.length());
+    }
+}
+//q6 
+public class StringDemo3 {
+    public static void main(String[] args) {
+        String str = new String("Golf is a good walk spoiled."); 
+        String sub = str.substring(29, 0); 
+
+        System.out.println("Substring: \"" + sub + "\"");
+        System.out.println("Length of substring: " + sub.length());
+    }
+}
+
+
