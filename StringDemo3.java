992ae8c638c6350c75317b6774663611759d665d
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-21 22:24:36 +0800

    chapter 11

--- StringDemo3.java
+++ StringDemo3.java
@@ -1,20 +1,8 @@
-public class StringDemo2 {
+public class StringDemo3 {
     public static void main(String[] args) {
-        String str = new String("Golf is a good walk spoiled."); // Create the original object
+        String str = new String("Golf is a good walk spoiled."); 
+        String sub = str.substring(8, 19); 
 
-        // Experiment with substring() method
-        String sub1 = str.substring(8, 19); // Create a new object from the original
-        String sub2 = str.substring(0, str.length()); // Same as str.substring(0, 28)
-        String sub3 = str.substring(0, 29); // Same as str.substring(0, 29)
-        String sub4 = str.substring(-1, 10); // First parameter negative
-        String sub5 = str.substring(10, 5); // Reverse the order of parameters
-
-        // Print results
-        System.out.println("Original string: \"" + str + "\"");
-        System.out.println("Substring (8, 19): \"" + sub1 + "\"");
-        System.out.println("Substring (0, str.length()): \"" + sub2 + "\"");
-        System.out.println("Substring (0, 29): \"" + sub3 + "\"");
-        System.out.println("Substring (-1, 10): \"" + sub4 + "\""); // This will throw an exception
-        System.out.println("Substring (10, 5): \"" + sub5 + "\""); // This will throw an exception
+        System.out.println(sub);
     }
 }
