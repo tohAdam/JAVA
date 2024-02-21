c80a42107d042f5359886abb29c2621234d19c99
tohAdam <139485068+tohAdam@users.noreply.github.com>
2024-02-21 22:23:07 +0800

    chapter 11

--- StringTrim.java
+++ StringTrim.java
@@ -1,13 +1,11 @@
-public class StringT {
+public class StringTrim {
     public static void main(String[] args) {
         String original = new String("    In a Hole    in the ground there lived a Hobbit.   ");
         String trimmed;
 
-        // Call trim() method to create a new String with leading and trailing spaces trimmed
         trimmed = original.trim();
 
-        // Print both original and trimmed Strings
         System.out.println("Original string: \"" + original + "\"");
-        System.out.println("Trimmed string:  \"" + trimmed + "\"");
+        System.out.println("Trimmed string: \"" + trimmed + "\"");
     }
 }
